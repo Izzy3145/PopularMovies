63107365e8e019b2631940bbc62962d0a9efc616
Izzy Stannett <isobel.stannett@gmail.com>
2018-03-03 18:19:35 +0000

    Up Navigation added, Settings menu added

--- app/src/main/java/com/example/android/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/example/android/popularmoviesstage1/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.android.popularmoviesstage1;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.Image;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -24,20 +27,21 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+
 import com.example.android.popularmoviesstage1.ImageAdapter.ImageAdapterClickHandler;
 
 
-public class MainActivity extends AppCompatActivity implements ImageAdapterClickHandler {
+public class MainActivity extends AppCompatActivity
+        implements ImageAdapterClickHandler, SharedPreferences.OnSharedPreferenceChangeListener {
 
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private ImageAdapter mAdapter;
+    private boolean SORT_BY;
 
     //example URL https://api.themoviedb.org/3/movie/popular?api_key=ccac7cd7a937bc204875001c4924f88a
     private static final String BASE_URL = "https://api.themoviedb.org";
     private static final String BASE_PATH = "3/movie";
-    private static final String BASE_QUERY = "popular?api_key=ccac7cd7a937bc204875001c4924f88a";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +57,9 @@ public class MainActivity extends AppCompatActivity implements ImageAdapterClick
         mAdapter = new ImageAdapter(this, new ArrayList<MovieItem>(), this);
         recyclerView.setAdapter(mAdapter);
 
+        setupSharedPreferences();
+
+        //TODO: move this method to NetworkUtils
         //check connectivity
         //ask Connectivity Manager to check the status of network connectivity
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -62,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements ImageAdapterClick
         if (networkInfo != null && networkInfo.isConnected()) {
             //call the Async method to make connection to given API in background
             MovieAsyncTask task = new MovieAsyncTask();
-            task.execute(buildUrl());
+            task.execute(buildUrl(SORT_BY));
         } else {
             //otherwise set the EmptyView to display an appropriate message
             //TODO: polish
@@ -85,7 +92,7 @@ public class MainActivity extends AppCompatActivity implements ImageAdapterClick
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings){
+        if (id == R.id.action_settings) {
             Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
             startActivity(startSettingsActivity);
             return true;
@@ -99,11 +106,41 @@ public class MainActivity extends AppCompatActivity implements ImageAdapterClick
         Intent startDetailActivity = new Intent(this, DetailActivity.class);
         startDetailActivity.putExtra("parcelledMovieItem", movieItem);
         startActivity(startDetailActivity);
+    }
+
+    //set up SharedPreferences and it's listener
+    private void setupSharedPreferences() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SORT_BY = sharedPreferences.getBoolean("sort_by_popularity", true);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        if (s.equals("sort_by_popularity")) {
+            SORT_BY = sharedPreferences.getBoolean("sort_by_popularity",true);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        android.support.v7.preference.PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    //method to build URL depending on preference
+    public static URL buildUrl(boolean sortBy) {
+        String BASE_QUERY_POPULAR = "popular?api_key=ccac7cd7a937bc204875001c4924f88a";
+        String BASE_QUERY_TOP_RATED = "top_rated?api_key=ccac7cd7a937bc204875001c4924f88a";
+        String BASE_QUERY;
+
+        if (sortBy) {
+            BASE_QUERY = BASE_QUERY_POPULAR;
+        } else {
+            BASE_QUERY = BASE_QUERY_TOP_RATED;
         }
 
-    //method to build URL
-    //TODO: build in sort order option
-    public static URL buildUrl() {
         Uri sortOrderUri = Uri.parse(BASE_URL).buildUpon()
                 .appendEncodedPath(BASE_PATH)
                 .appendEncodedPath(BASE_QUERY)
@@ -146,4 +183,5 @@ public class MainActivity extends AppCompatActivity implements ImageAdapterClick
             //TODO: polish, if no results, set EmptyTextView
             mAdapter.setMovieData(queriedMovieItems);
         }
-    }}
+    }
+}
